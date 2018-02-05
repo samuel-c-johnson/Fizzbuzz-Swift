81b37da3c3c987053b8bd3e53c3efbe89429d0e1
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-02-05 16:19:07 +0000

    Creates fizz buzz file

---
+++ FizzBuzz/Fizzbuzz.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  FizzBuzz
+//
+//  Created by Samuel Johnson on 05/02/2018.
+//  Copyright © 2018 Samuel Johnson. All rights reserved.
+//
+
+import Foundation
+
+class fizzBuzz {
+    
+}
